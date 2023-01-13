52cfb546e2db699a8e3c70695c5636f00511f555
Aleyna <aleyyy416@gmail.com>
2023-01-13 16:22:48 +0300

    müşteri ekleme veritabanına bağlandı.

--- WindowsFormsApp2/musteri_ekle.cs
+++ WindowsFormsApp2/musteri_ekle.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
 
 namespace WindowsFormsApp2
 {
@@ -16,10 +18,31 @@ namespace WindowsFormsApp2
         {
             InitializeComponent();
         }
-
+        SqlConnection baglanti = new SqlConnection("Data Source=ALEYY;Initial Catalog=market;Integrated Security=True");
         private void label3_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into müşteri1(müşteri_no,müşteriAdi,müşteriSoyadi) values(@müşteri_no,@müşteriAdi,@müşteriSoyadi)");
+            komut.Parameters.AddWithValue("@müşteri_no", textBox1.Text);
+            komut.Parameters.AddWithValue("@müşteriAdi", textBox2.Text);
+            komut.Parameters.AddWithValue("@müşteriSoyadi", textBox3.Text);
+            //komut.ExecuteNonQuery();
+            //baglanti.Close();
+            MessageBox.Show("Yeni müşteri eklendi.");
+            baglanti.Close();
+            foreach (Control item in this.Controls)
+            {
+                if (item is TextBox)
+                {
+                    item.Text = " ";
+                }
+
+            }
         }
     }
 }
