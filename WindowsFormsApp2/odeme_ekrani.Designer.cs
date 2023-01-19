41b44725cddc2552125f32a99767c2bbf6bb9612
Aleyna <aleyyy416@gmail.com>
2023-01-20 00:24:42 +0300

    müşteri no araması eklendi

--- WindowsFormsApp2/odeme_ekrani.Designer.cs
+++ WindowsFormsApp2/odeme_ekrani.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -36,15 +37,25 @@
             this.button2 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.marketDataSet = new WindowsFormsApp2.marketDataSet();
+            this.müsteri1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.müsteri1TableAdapter = new WindowsFormsApp2.marketDataSetTableAdapters.müsteri1TableAdapter();
+            this.müsteriborcDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.marketDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.müsteri1BindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.BackColor = System.Drawing.Color.Sienna;
             this.label1.Font = new System.Drawing.Font("Franklin Gothic Heavy", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
-            this.label1.Location = new System.Drawing.Point(-3, -1);
+            this.label1.Location = new System.Drawing.Point(-4, -1);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(774, 42);
+            this.label1.Size = new System.Drawing.Size(1032, 52);
             this.label1.TabIndex = 0;
             this.label1.Text = "                                          ÖDEME EKRANI";
             // 
@@ -52,9 +63,10 @@
             // 
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Arial", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
-            this.label2.Location = new System.Drawing.Point(38, 112);
+            this.label2.Location = new System.Drawing.Point(179, 154);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(109, 23);
+            this.label2.Size = new System.Drawing.Size(145, 28);
             this.label2.TabIndex = 1;
             this.label2.Text = "Müşteri No :";
             // 
@@ -62,9 +74,10 @@
             // 
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Arial", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
-            this.label3.Location = new System.Drawing.Point(38, 172);
+            this.label3.Location = new System.Drawing.Point(179, 228);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(153, 23);
+            this.label3.Size = new System.Drawing.Size(204, 28);
             this.label3.TabIndex = 2;
             this.label3.Text = "Ödenecek Tutar : ";
             // 
@@ -72,9 +85,10 @@
             // 
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Arial", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
-            this.label4.Location = new System.Drawing.Point(38, 242);
+            this.label4.Location = new System.Drawing.Point(179, 314);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(128, 23);
+            this.label4.Size = new System.Drawing.Size(171, 28);
             this.label4.TabIndex = 3;
             this.label4.Text = "Güncel Borç : ";
             // 
@@ -82,48 +96,102 @@
             // 
             this.button1.BackColor = System.Drawing.Color.Sienna;
             this.button1.Font = new System.Drawing.Font("Franklin Gothic Heavy", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
-            this.button1.Location = new System.Drawing.Point(392, 107);
+            this.button1.Location = new System.Drawing.Point(651, 148);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 33);
+            this.button1.Size = new System.Drawing.Size(144, 41);
             this.button1.TabIndex = 4;
             this.button1.Text = "ARA";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.Sienna;
             this.button2.Font = new System.Drawing.Font("Arial Black", 12F);
             this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button2.Location = new System.Drawing.Point(392, 167);
+            this.button2.Location = new System.Drawing.Point(651, 222);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 31);
+            this.button2.Size = new System.Drawing.Size(144, 38);
             this.button2.TabIndex = 5;
             this.button2.Text = "ÖDE";
             this.button2.UseVisualStyleBackColor = false;
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(208, 107);
+            this.richTextBox1.Location = new System.Drawing.Point(405, 148);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(146, 28);
+            this.richTextBox1.Size = new System.Drawing.Size(193, 34);
             this.richTextBox1.TabIndex = 7;
             this.richTextBox1.Text = "";
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(208, 167);
+            this.richTextBox2.Location = new System.Drawing.Point(405, 222);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(146, 28);
+            this.richTextBox2.Size = new System.Drawing.Size(193, 34);
             this.richTextBox2.TabIndex = 8;
             this.richTextBox2.Text = "";
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.müsteriborcDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.müsteri1BindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(405, 296);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(193, 46);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // marketDataSet
+            // 
+            this.marketDataSet.DataSetName = "marketDataSet";
+            this.marketDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // müsteri1BindingSource
+            // 
+            this.müsteri1BindingSource.DataMember = "müsteri1";
+            this.müsteri1BindingSource.DataSource = this.marketDataSet;
+            // 
+            // müsteri1TableAdapter
+            // 
+            this.müsteri1TableAdapter.ClearBeforeFill = true;
+            // 
+            // müsteriborcDataGridViewTextBoxColumn
+            // 
+            this.müsteriborcDataGridViewTextBoxColumn.DataPropertyName = "müsteri_borc";
+            this.müsteriborcDataGridViewTextBoxColumn.HeaderText = "müsteri_borc";
+            this.müsteriborcDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.müsteriborcDataGridViewTextBoxColumn.Name = "müsteriborcDataGridViewTextBoxColumn";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Sienna;
+            this.button3.Font = new System.Drawing.Font("Franklin Gothic Heavy", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.button3.Location = new System.Drawing.Point(651, 296);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(144, 40);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "GÜNCELLE";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
             // odeme_ekrani
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Honeydew;
             this.BackgroundImage = global::WindowsFormsApp2.Properties.Resources.login2;
-            this.ClientSize = new System.Drawing.Size(769, 442);
+            this.ClientSize = new System.Drawing.Size(1025, 544);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button2);
@@ -132,8 +200,13 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "odeme_ekrani";
             this.Text = "odeme_ekrani";
+            this.Load += new System.EventHandler(this.odeme_ekrani_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.marketDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.müsteri1BindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -148,5 +221,11 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private marketDataSet marketDataSet;
+        private System.Windows.Forms.BindingSource müsteri1BindingSource;
+        private marketDataSetTableAdapters.müsteri1TableAdapter müsteri1TableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn müsteriborcDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button button3;
     }
 }
--- WindowsFormsApp2/odeme_ekrani.cs
+++ WindowsFormsApp2/odeme_ekrani.cs
@@ -7,14 +7,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace WindowsFormsApp2
 {
     public partial class odeme_ekrani : Form
     {
+        SqlConnection con;
+        SqlDataReader dr;
+        SqlCommand com;
         public odeme_ekrani()
         {
             InitializeComponent();
+        }
+
+        private void odeme_ekrani_Load(object sender, EventArgs e)
+        {
+            // TODO: Bu kod satırı 'marketDataSet.müsteri1' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.müsteri1TableAdapter.Fill(this.marketDataSet.müsteri1);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user = richTextBox1.Text;
+            con = new SqlConnection("Data Source=ALEYY;Initial Catalog=market;Integrated Security=True");
+            com = new SqlCommand();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "Select * from müsteri1 where müsteri_no = '" + richTextBox1.Text + "'";
+
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                MessageBox.Show("Müşteri bulundu. ");
+                Form2 fr2 = new Form2();
+                fr2.ShowDialog();
+
+            }
+            else
+            {
+                MessageBox.Show("Hatalı giris. Müşteri bulunamadı.");
+            }
+            con.Close();
         }
     }
 }
